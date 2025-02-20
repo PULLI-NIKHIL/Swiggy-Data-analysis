be3741181e865d81d9c700c594bd196c76eb6378
PULLI-NIKHIL <nikhilpulli546@gmail.com>
2025-02-20 16:35:17 +0530

    Update Swiggy data analysis 2.0.sql

--- Swiggy data analysis 2.0.sql
+++ Swiggy data analysis 2.0.sql
@@ -1,19 +1,2 @@
 ﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# SQL Server Management Studio Solution File, Format Version 20.00
-VisualStudioVersion = 15.0.28307.421
-MinimumVisualStudioVersion = 10.0.40219.1
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Default|Default = Default|Default
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{3E7966CF-256F-4309-A756-CE0F58F77EBF}.Default|Default.ActiveCfg = Default
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {38928A64-4E12-4312-B248-0753D596A2BC}
-	EndGlobalSection
-EndGlobal
+
